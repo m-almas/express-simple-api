5451305602b95cddb1a6dd4a06fbc032c25fca07
Pupye <fullhouseya89@gmail.com>
2019-12-26 16:17:26 +0600

    two different db's for test and development

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const postsRoute = require('./routes/posts');
 const authRoute = require('./routes/auth');
 
 //Middleware
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use('/user', authRoute);
 app.use('/posts', postsRoute);
@@ -20,10 +20,11 @@ app.get('/', (req, res) => {
 });
 
 
-
+const db_connectionStr = process.env.NODE_ENV === "development" ?
+    process.env.DB_CONNECTION_DEV : process.env.DB_CONNECTION_TEST
 //Connect to DB
 mongoose.connect(
-    process.env.DB_CONNECTION,
+    db_connectionStr,
     { useUnifiedTopology: true, useNewUrlParser: true },
     () => console.log('connected to db')
 )
