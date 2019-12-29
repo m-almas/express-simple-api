908a23644262b2edc5c25152239667771870d333
Pupye <fullhouseya89@gmail.com>
2019-12-29 17:26:19 +0600

    exported authController

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,9 @@
 //import all relevant controllers here and export from one place 
 const postController = require('./postController')
 const userController = require('./userController')
+const authController = require('./authController')
 module.exports = {
     postController, 
-    userController
+    userController, 
+    authController, 
 }
