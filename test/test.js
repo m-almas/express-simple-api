47b09f51ceb3db13bc9445506738bb61e7eb36be
Pupye <fullhouseya89@gmail.com>
2019-12-29 14:44:12 +0600

    new test on get all

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ process.env.NODE_ENV = "test"
 
 let mongoose = require('mongoose')
 let Post = require('../models/Post')
-
+const {getExamplePost} = require('../services/postService')
 const chai = require('chai')
 const chaiHttp = require('chai-http')
 const { app } = require('../app')
@@ -17,7 +17,7 @@ describe('Posts', () => {
         })
     })
     describe('/GET Post', () => {
-        it('should get all posts', (done) => {
+        it('should check initial number of posts', (done) => {
             chai.request(app)
                 .get('/posts')
                 .send({})
@@ -29,8 +29,28 @@ describe('Posts', () => {
                     res.body.posts.length.should.be.eql(0)
                     done()
                 })
-        }
-        )
+        })
+
+        it('should save posts and get them', (done) => {
+            const post = new Post(getExamplePost())
+            post.save().then((savedPost) => {
+                chai.request(app)
+                .get('/posts')
+                .send({})
+                .end((err,res)=>{
+                    res.should.have.status(200)
+                    res.body.should.be.a('object')
+                    res.body.should.have.property('posts')
+                    res.body.posts.should.be.a('array')
+                    res.body.posts.length.should.be.eql(1)
+                    const actualPost = res.body.posts[0]
+                    actualPost.should.have.property('title')
+                    actualPost.title.should.be.eql(savedPost.title)
+                    actualPost.description.should.be.eql(savedPost.description)
+                    done()
+                })
+            })
+        })
     })
     describe('/POST posts', async () => {
         let testCases = [
