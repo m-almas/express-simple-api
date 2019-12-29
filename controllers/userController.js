481cfd281caee072ef84b053135bbba27813ba3d
Pupye <fullhouseya89@gmail.com>
2019-12-29 15:04:02 +0600

    created controller for user

---
+++ controllers/userController.js
@@ -0,0 +1,12 @@
+const {userService} = require('../services/index')
+const {UnexpectedErr} = require('../errors/index')
+
+const getUserById = async (req, res, next) => {
+    try {
+        const user = await userService.getUserById(req.params.userId)
+        res.json({user: user})
+        next()
+    } catch (error) {
+        UnexpectedErr(res, error)
+    }
+}
