932d37ef6c3080f5d5525d70cecb7d2a193690cd
Pupye <fullhouseya89@gmail.com>
2019-12-29 18:48:32 +0600

    formatted authController

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,9 +1,9 @@
-const {registerValidation} = require('./validation/authValidation')
-const {userService} = require('../services/index')
-const {InternalServerErr, ValidationFailedErr} = require('../errors/index')
+const { registerValidation } = require('./validation/authValidation')
+const { userService } = require('../services/index')
+const { InternalServerErr, ValidationFailedErr } = require('../errors/index')
 
 const registerUser = async (req, res, next) => {
-    const user = req.body 
+    const user = req.body
     try {
         await registerValidation(user)
     } catch (error) {
@@ -13,13 +13,13 @@ const registerUser = async (req, res, next) => {
     //validated
     try {
         const registered = await userService.createUser(user)
+        res.json(registered)
+        next()
     } catch (error) {
         InternalServerErr(res, error)
         return
     }
-    
-    res.json(registered)
-    next()
+
 }
 
 module.exports = {
