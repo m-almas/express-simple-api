d17b49239d497912b31d355c299c8b81a7cf0fb0
Pupye <fullhouseya89@gmail.com>
2019-12-29 16:46:28 +0600

    comment explaining user controller

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const controller = require('../controllers/index')
 const {userController} = controller
-
+//this controller might be needed for admin panel
 
 router.get('/:userId', userController.getUserById)
 
